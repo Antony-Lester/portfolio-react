c858238c55d46028a13961b7151fb16fdd049734
Antony-Lester <me@antonylester.com>
2022-11-13 00:21:03 +0000

    firebase setup

--- src/controllers/menu-right.jsx
+++ src/controllers/menu-right.jsx
@@ -2,7 +2,11 @@ import '../styling/menu.css';
 import '../styling/menu-right.css';
 
 import { menuItemsRight } from '../utils/menu-items'
+import { Link } from 'react-router-dom';
 
 export default function MainRight() {
-    return (<div className='menu-right'>{menuItemsRight.map((e, i) => <img key={e.alt} src={e.src} alt="" className='menu menu-right-item' />)}</div>)
+    return (<div className='menu-right'>
+        {menuItemsRight.map((e, i) => 
+        <Link to={e.path}><img key={e.alt + i} src={e.src} alt="" className='menu menu-left-item'/></Link>  
+        )}</div>)
 }
