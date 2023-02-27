258d84ed13a62a4ec55a0cb98577f4a2b27149e5
Antony-Lester <me@antonylester.com>
2023-02-27 03:32:54 +0000

    northcoders certificate added

--- src/controllers/Certs.jsx
+++ src/controllers/Certs.jsx
@@ -1,11 +1,12 @@
 import freeCodeCamp from '../images/certs/js_cert.png'
 import playStore from '../images/certs/play_store_cert.png'
-
+import northcoders from '../images/certs/northcoders.png'
 import '../styling/Certs.css'
 
 export default function Certs() { 
     return (
         <div className='main border scroll'>
+            <img className="cert" src={northcoders} alt="northcoders bootcamp cert" />
             <a href="https://www.freecodecamp.org/certification/fcc9288cd5c-bf80-4647-928c-882c4fc224c3/javascript-algorithms-and-data-structures">
                     <img className="cert" src={freeCodeCamp} alt="free code camp js cert"/>
             </a>
